7962115e45819c716a3aa45dd4c99f7b6a59105f
George Foot <george.foot@gmail.com>
2023-02-05 06:59:23 +0000

    Fix the memory layout summarizer

--- py/summary.py
+++ py/summary.py
@@ -18,16 +18,18 @@ def report(name, start, end):
 syms = parse_syms("labels/supshad.labels")
 
 report("Normal stubs", "normal_stubs_dest", "normal_stubs_end")
-report("Normal lang w/s code", "lang_ws_dest", "lang_ws_end")
-report("Normal inbuffer", "normal_inbuffer", None)
+report("Normal lang w/s code low", "lang_ws_dest", "lang_ws_low_end")
+report("Normal lang w/s code high", "lang_ws_high_start", "lang_ws_end")
+report("Normal transfer buffer", "normal_transfer_buffer", None)
 
 print("")
 
 report("Shadow stubs", "shadow_stubs_dest", "shadow_stubs_end")
 report("Shadow ZP vars", "shadow_zpvars", 0x100)
-report("Shadow inbuffer", "shadow_inbuffer", None)
-report("Shadow OS", "shadow_code_dest", "shadowos_top")
-report("Shadow vectors", "osrdrm", 0x10000)
+report("Shadow transfer buffer", "shadow_transfer_buffer", None)
+report("Shadow OS low", "shadow_code_low_dest", "shadow_code_low_top")
+report("Shadow OS high", "shadow_code_high_dest", "shadowos_top")
+report("Shadow vectors", "osrdrm", "shadow_code_high_dest_end")
 
 print("")
 
@@ -37,5 +39,5 @@ report("Bootup", "bootup_begin", "bootup_end")
 report("Lang w/s source", "lang_ws_source", "lang_ws_source_end")
 report("Shadow stubs source", "shadow_stubs_source", "shadow_stubs_source_end")
 report("Normal stubs source", "normal_stubs_source", "normal_stubs_source_end")
-report("Shadow OS source", "shadow_code_source", "shadow_code_source_end")
+report("Shadow OS source", "shadow_code_low_source", "shadow_code_high_source_end")
 
